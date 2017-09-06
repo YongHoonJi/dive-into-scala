3b317d3af0e6182268181c485d1dfca8d6ff8ed6
jyh <yonghoon.ji@systrangroup.com>
2017-09-06 11:30:07 +0900

    init

---
+++ diveInScala/src/PageReader.scala
@@ -0,0 +1,24 @@
+import scala.io.Source
+
+class PageReader(){
+
+  val fileEquals = """[A-z]{1}:.+""".r
+  val httpEquals = """http://.+""".r
+
+  def readFile(uri: String): Option[Iterator[String]] = uri match {
+    case fileEquals() =>
+      try {
+        println(uri)
+        Some(Source.fromFile(uri).getLines())
+      }
+      catch {
+        case e: Exception => None
+      }
+
+    case httpEquals() =>
+      None
+
+    case _ =>
+      None
+  }
+}
---
+++ diveInScala/src/test_intellij.sc
@@ -0,0 +1,5 @@
+object test_intellij {
+  val wc = new PageReader()                       //> wc  : PageReader = PageReader@3b6eb2ec
+  wc.readFile("F:\\TEMP\\scala\\test.txt")        //> F:\TEMP\scala\test.txt
+                                                  //| res0: Option[Iterator[String]] = Some(non-empty iterator)
+}
